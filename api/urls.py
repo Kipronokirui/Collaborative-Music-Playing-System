6a2661d4f53aee285b2056402c88f1de55face7b
Kipronokirui <victorkipronokirui2@gmail.com>
2023-05-12 19:21:20 +0300

    frontend

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateRoomView, RoomView
+from .views import CreateRoomView, RoomView, GetRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view())
 ]
